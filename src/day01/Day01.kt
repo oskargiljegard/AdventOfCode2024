6c72044cbbdc5e7ce4a74a320534ec613f8f62ca
Oskar Giljegard <oskar.giljegard@cpacsystems.se>
2024-12-01 06:17:26 +0100

    finished day 1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -4,5 +4,17 @@ import java.io.File
 
 fun main() {
     val lines = File("src/day01/input.txt").readLines()
-    println(lines.size)
+    val parsed = lines.map { line -> line.split("   ").map { it.toInt() } }
+    val left = parsed.map { pair -> pair[0] }.sorted()
+    val right = parsed.map { pair -> pair[1] }.sorted()
+    val differences = left.zip(right).map { Math.abs(it.first - it.second) }
+    println("part 1")
+    println(differences.sum())
+
+    println("part 2")
+    val similarities = left.map { leftVal ->
+        leftVal * right.count { it == leftVal }
+    }
+    println(similarities.sum())
+
 }
