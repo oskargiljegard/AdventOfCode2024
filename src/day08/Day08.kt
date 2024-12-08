0d52be553e20fb4930f446e7f6276d7c9e63a5a3
Oskar Giljegard <oskar@giljegard.com>
2024-12-08 06:36:53 +0100

    refactored day 8

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -11,19 +11,22 @@ fun main() {
     val grid = input.asCharGrid { it }
     grid.toStringIndexed { pos, c -> c.toString() }.also { println(it) }
     val types = grid.positions.map { grid[it] }.filter { it != '.' }.toSet().toList()
-    var total = 0
-    val allAntiNodes = mutableListOf<Vector>()
-    for (type in types) {
-        val antennas = grid.positions.filter { grid[it] == type }
-        val antennaPairs = antennas.product(antennas).filter { (a, b) -> a != b }
-        val antiNodes = antennaPairs.flatMap { (a, b) -> generateSequence(b, { it + (b - a) }).takeWhile { it.isWithinGrid(grid) } }
-        allAntiNodes.addAll(antiNodes)
-    }
-    val antiNodesInMap =
-        allAntiNodes.map { Vector(it.intX, it.intY) }.toSet()
-            .filter { it.intX >= 0 && it.intY >= 0 && it.intX <= grid.maxPos.intX && it.intY <= grid.maxPos.intY }
-    println(antiNodesInMap.size)
+    getAllAntiNodesInMap(grid, types) { drop(1).take(1) }.also { println(it) }
+    getAllAntiNodesInMap(grid, types) { takeWhile { it.isWithinGrid(grid) }}.also { println(it) }
 }
 
 fun Vector.isWithinGrid(grid: Grid<Char>) = intX >= 0 && intY >= 0 && intX <= grid.maxPos.intX && intY <= grid.maxPos.intY
 
+fun getAllAntiNodesInMap(grid: Grid<Char>, types: List<Char>, limitAntiNodes: Sequence<Vector>.() -> Sequence<Vector>): Int {
+    val allAntiNodes = mutableListOf<Vector>()
+    for (type in types) {
+        val antennas = grid.positions.filter { grid[it] == type }
+        val antennaPairs = antennas.product(antennas).filter { (a, b) -> a != b }
+        val antiNodes = antennaPairs.flatMap { (a, b) -> generateSequence(b) { it + (b - a) }.limitAntiNodes() }
+        allAntiNodes.addAll(antiNodes)
+    }
+    val allAntiNodesInMap =
+        allAntiNodes.map { Vector(it.intX, it.intY) }.toSet()
+            .filter { it.isWithinGrid(grid) }
+    return allAntiNodesInMap.size
+}
