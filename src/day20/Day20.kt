5e90c1334ee78b0fb5afa7685c65b8dfd3f6e5b4
Oskar Giljegard <oskar@giljegard.com>
2024-12-20 22:21:29 +0100

    finished day 20

--- src/day20/Day20.kt
+++ src/day20/Day20.kt
@@ -3,6 +3,8 @@ package day20
 import day18.isWithinGrid
 import utils.*
 import java.io.File
+import java.util.LinkedList
+import java.util.Queue
 import kotlin.math.abs
 
 val dirs = listOf(Vector(1, 0), Vector(0, -1), Vector(-1, 0), Vector(0, 1))
@@ -14,58 +16,45 @@ fun main() {
     val endPos = grid.positions.find { grid[it] == 'E' }!!
     grid[startPos] = '.'
     grid[endPos] = '.'
-    //grid.toStringIndexed { pos, c -> c.toString() }.also { println(it) }
-    println(grid.positions.filter { grid[it] == '#' }.size)
-    val origPath = getPath(grid, startPos, endPos)
+    grid.toStringIndexed { pos, c -> c.toString() }.also { println(it) }
+    println()
+    val distGrid = makeDistGrid(grid, endPos)
+    distGrid.toStringIndexed { pos, c -> c?.toString() ?: "." }.also { println(it) }
 
-    val lenGrid = Grid<Int?>(grid.size) { pos -> null }
-    for ((i, p) in (listOf(startPos) + origPath).withIndex()) {
-        lenGrid[p] = i
+    val cheats = distGrid.positions.filter { distGrid[it] != null }.flatMap { from ->
+        distGrid.positions.filter { distGrid[it] != null }.mapNotNull inner@{ to ->
+            val dist = abs(from.intX - to.intX) + abs(from.intY - to.intY)
+            if (dist > 20) {
+                return@inner null
+            }
+            val saving = distGrid[from]!! - distGrid[to]!! - dist
+            if (saving < 100) {
+                return@inner null
+            }
+            return@inner Triple(from, to, saving)
+        }
     }
-    lenGrid.positions.filter { grid[it] == '#' }.filter { pos -> dirs.product(dirs).any { (d1, d2) ->
-        val p1 = pos + d1
-        val p2 = pos + d2
-        if (!p1.isWithinGrid(grid) || !p2.isWithinGrid(grid)) {
-            return@any false
-        }
-        val v1 = lenGrid[p1]
-        val v2 = lenGrid[p2]
-        if (v1 == null || v2 == null) {
-            return@any false
-        }
-        return@any abs(v1 - v2) >= 102
-    } }.size.also { println(it) }
-
-    /*
-    val possibleCheats =
-        (listOf(startPos) + origPath).flatMap { pos -> dirs.map { pos + it } }.filter { it.isWithinGrid(grid) }
-            .filter { grid[it] == '#' }.toSet()
-    println(possibleCheats.size)
-    var numCheats = 0
-    for ((i, cheat) in possibleCheats.withIndex()) {
-        grid[cheat] = '.'
-        val len = getPath(grid, startPos, endPos).size
-        if (len <= origPath.size - 100) {
-            numCheats++
-        }
-        grid[cheat] = '#'
-        if (i % 100 == 0) println(i)
-    }
-    println(numCheats)
-     */
+    println(cheats.size)
+    //println(cheats.map { (from, to, saving) -> saving }.groupingBy { it }.eachCount())
 }
 
-fun getPath(grid: Grid<Char>, startPos: Vector, endPos: Vector): List<Vector> {
-    val graph = HashmapGraph(
-        grid.positions.filter { grid[it] == '.' }.map { pos ->
-            pos to dirs.map { pos + it }.filter { it.isWithinGrid(grid) }.filter { grid[it] == '.' }
-        }.toMap()
-    )
-    val path = graph.aStarShortestPath(
-        startPos,
-        endPos,
-        { from, to -> 1.0 },
-        { from -> abs(from.x - endPos.x) + abs(from.y - endPos.y) }
-    )!!
-    return path
+fun makeDistGrid(grid: Grid<Char>, endPos: Vector): Grid<Int?> {
+    val distGrid: Grid<Int?> = gridOf(grid.size, null)
+    val closed: MutableSet<Vector> = mutableSetOf()
+    val open: Queue<Vector> = LinkedList()
+    open.add(endPos)
+    distGrid[endPos] = 0
+    while (open.isNotEmpty()) {
+        val pos = open.remove()
+        closed.add(pos)
+        val neighbors = dirs.map { dir -> pos + dir }
+            .filter { it.isWithinGrid(grid) }
+            .filter { grid[it] == '.' }
+            .filter { !closed.contains(it) }
+        for (n in neighbors) {
+            distGrid[n] = distGrid[pos]!! + 1
+            open.add(n)
+        }
+    }
+    return distGrid
 }
