b49eb2d60b507e9fb466d7aea6973b5bcfb3e0b9
Oskar Giljegard <oskar@giljegard.com>
2024-12-27 18:05:24 +0100

    refactored day 1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,13 +1,14 @@
 package day01
 
 import java.io.File
+import kotlin.math.abs
 
 fun main() {
     val lines = File("src/day01/input.txt").readLines()
     val parsed = lines.map { line -> line.split("   ").map { it.toInt() } }
     val left = parsed.map { pair -> pair[0] }.sorted()
     val right = parsed.map { pair -> pair[1] }.sorted()
-    val differences = left.zip(right).map { Math.abs(it.first - it.second) }
+    val differences = left.zip(right).map { abs(it.first - it.second) }
     println("part 1")
     println(differences.sum())
 
